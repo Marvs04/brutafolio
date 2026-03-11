fe86bb53121700c4c5c658a81b7fefa077d0d934
Marvs04 <marvinfrancisco97@gmail.com>
2026-03-10 21:40:39 -0600

    feat: add Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { SpeedInsights } from '@vercel/speed-insights/react';
+import { Analytics } from '@vercel/analytics/react';
 import App from './App.tsx';
 import './index.css';
 import { LanguageProvider } from './context/LanguageContext.tsx';
@@ -12,6 +14,8 @@ createRoot(document.getElementById('root')!).render(
       <LanguageProvider>
         <BlueprintProvider>
           <App />
+          <SpeedInsights />
+          <Analytics />
         </BlueprintProvider>
       </LanguageProvider>
     </BrowserRouter>
